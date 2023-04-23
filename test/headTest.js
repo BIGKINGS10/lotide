ef267dc18473d60d314c14528aff479501460be6
BIGKINGS10 <adventbigkings@gmail.com>
2023-04-23 10:22:45 +0100

    Install Mocha and Chai and refactored headTest.js with M & C framework for more readiable testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,25 @@
-const assertEqual = require('../assertEqual');
-const head = require ('../head');
-  // TEST CODE
-  assertEqual(head([5,6,7]), 5);
-  assertEqual(head([5,6,7]), 4);
-  assertEqual(head([]), 4);
-  assertEqual(head([4]), 4);
-  assertEqual(head([5]), 4);
-  assertEqual(head(["Hello", "lighthouse", "Labs"]), "Hello");
-  assertEqual(head(["Hello", "lighthouse", "Labs"]), "hello");
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns '5' for ['5,6,7']", () => {
+    assert.strictEqual(head(['5', 6, 7]), '5'); 
+  });
+
+  it("returns 'hello' for ['Hello', 'lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'lighthouse', 'Labs']), 'Hello'); 
+  });
+
+  
+
+});
+
+
