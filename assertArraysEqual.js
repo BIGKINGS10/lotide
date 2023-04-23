50eb48cdd7c73596302020561fb8fa5ddc1097d3
BIGKINGS10 <adventbigkings@gmail.com>
2023-04-23 09:58:36 +0100

    Refactored middle.js and created middleTest.js file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,28 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  
-
-    let equal = true;
-    //Test to see if arr1 has the same length as arr2
-    if (arr1.length === arr2.length) {
-      // if the length of arr1 and arr2 are the same, test to see if arr1[index] == arr2[index]
-      for (let i = 0; i < arr1.length; i++) {
-        if (Array.isArray(arr1[i])) {
-          equal =  eqArrays(arr1[i], arr2[i]);
-        } else if (arr1[i] === arr2[i]) {
-          equal = true;
-        } else {
-          equal = false;
-          return equal;
-        }
-      }
-    } else {
-      equal = false;
-      return equal;
-    }
-  
-    return equal;
-  };
-  
+const eqArrays = require('./eqArrays');
   
  const assertArraysEqual = function(arr1, arr2) {
   // input: two arrays
@@ -41,10 +17,6 @@ const eqArrays = function(arr1, arr2) {
   console.log(result);
 };
 
-  assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-
-
-
-  module.exports = assertArraysEqual;
+module.exports = assertArraysEqual;
 
   
